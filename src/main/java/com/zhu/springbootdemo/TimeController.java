b3fc42141d244fa73425c4de1f5e313b55dddff4
zhuzidian <zhuzidian@gmail.com>
2024-01-22 13:32:40 +0900

    update

--- src/main/java/com/zhu/springbootdemo/TimeController.java
+++ src/main/java/com/zhu/springbootdemo/TimeController.java
@@ -2,10 +2,13 @@ package com.zhu.springbootdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 // @RequestMapping("time")
@@ -14,14 +17,15 @@ public class TimeController {
     Logger log = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("now")
-    public String now() {
-        var a = 888;
+    public Map now() {
         log.trace("trace");
         log.debug("debug");
         log.info("info");
         log.warn("warn");
         log.error("error");
 
-        return java.time.LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        // WebProperties
+        var now = java.time.LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        return Map.of("time", now);
     }
 }
